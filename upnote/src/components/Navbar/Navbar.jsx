0b5a1aa87b3a8cc4f6092f59f17657eee988e648
HYE <jelly12paw@gmail.com>
2024-01-12 14:30:56 +0900

    chore: search bar button

--- upnote/src/components/Navbar/Navbar.jsx
+++ upnote/src/components/Navbar/Navbar.jsx
@@ -5,15 +5,17 @@ import { TbBoxMultiple } from "react-icons/tb";
 
 export default function Navbar() {
     return (
-        <div className='w-screen p-4'>
+        <nav className='w-screen p-4'>
             <div className='flex justify-between px-4'>
                 <div className='text-2xl flex items-center'>
                     <AiOutlineMenu className='mr-6' />
                     <AiOutlineLeft className='mr-4' />
                     <AiOutlineRight className='mr-8' />
                     <IoMdRefresh className='mr-8'/>
-                    <AiOutlineSearch />
-                    <input type="search" placeholder='  Search' className='h-8 w-80 text-lg border border-gray-300 rounded' />
+                    <div className='h-10 w-80 border border-gray-300 rounded flex items-center'>
+                        <button className='ml-2 text-2xl'><AiOutlineSearch /></button>
+                        <input type="text" placeholder='  Search' className='text-xl' />
+                    </div>
                 </div>
                 <div className='text-2xl flex items-center'>
                     <button className='bg-blue-700 hover:bg-blue-800 text-white py-2 px-4 rounded text-lg mr-8'>New Note</button>
@@ -21,7 +23,7 @@ export default function Navbar() {
                     <AiOutlineSetting />
                 </div>
             </div>
-        </div>
+        </nav>
     );
 }
 
